b3babc44290bbe4333e801dddea8b3f7b0b724ab
Vitor Campos <vitorcampos742@gmail.com>
2021-12-06 19:17:12 -0300

    requisito 9

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,7 +1,8 @@
 const url1 = 'https://api.mercadolibre.com/items/';
 
 const fetchItem = (id) => fetch(`${url1}${id}`)
-.then((response) => response.json());
+.then((response) => response.json())
+.catch(() => new Error('You must provide an url'));
 
 if (typeof module !== 'undefined') {
   module.exports = {
